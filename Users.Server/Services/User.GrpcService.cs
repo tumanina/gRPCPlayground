23a01713d00ae481fc8c8289a25e7427e134097d
HOME-PC\Olga <tumanina@yandex.ru>
2026-06-01 00:12:23 +0300

    Implement sample of users grpc service

---
+++ Users.Server/Services/User.GrpcService.cs
@@ -0,0 +1,56 @@
+﻿using Grpc.Core;
+
+namespace Users.Server.Services
+{
+    public class UserGrpcService : UserService.UserServiceBase
+    {
+        public override Task<UserResponse> GetById(
+            GetUserByIdRequest request,
+            ServerCallContext context)
+        {
+            return Task.FromResult(new UserResponse
+            {
+                User = new User
+                {
+                    Id = request.Id,
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Email = "john.doe@gmail.com"
+                }
+            });
+        }
+
+        public override Task<GetAllUsersResponse> GetAll(
+            GetAllUsersRequest request,
+            ServerCallContext context)
+        {
+            var response = new GetAllUsersResponse();
+
+            response.Users.Add(new User
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "john@test.com"
+            });
+
+            return Task.FromResult(response);
+        }
+
+        public override Task<UserResponse> Create(
+            CreateUserRequest request,
+            ServerCallContext context)
+        {
+            return Task.FromResult(new UserResponse
+            {
+                User = new User
+                {
+                    Id = 123,
+                    FirstName = request.FirstName,
+                    LastName = request.LastName,
+                    Email = request.Email
+                }
+            });
+        }
+    }
+}
---
+++ UsersClient/Program.cs
@@ -0,0 +1,16 @@
+using Grpc.Net.Client;
+using Users.Client;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7085");
+
+var client = new UserService.UserServiceClient(channel);
+
+var response = await client.GetByIdAsync(
+    new GetUserByIdRequest
+    {
+        Id = 1
+    });
+
+Console.WriteLine(response.User.FirstName);
+
+Console.ReadLine();
